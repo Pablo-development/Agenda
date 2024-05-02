8176405de496ac65d2a67de97ffd2a2e6829baf0
Pablo <pablo.mikolaiczyki@gmail.com>
2024-05-02 14:55:49 -0300

    adicionada função de autenticação

--- core/views.py
+++ core/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
+from django.contrib import messages
 
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-
+@login_required(login_url='/login/') #caso nao possua autenticação, sera direcionado para 'login'
 def lista_eventos(request):
-    evento = Evento.objects.all()#puxando todos os eventos do banco de dados
-    #há a possibilidade de filtrar os eventos por meio de usuarios da seguinte forma:
     usuario = request.user
-    #evento = Evento.objects.filter(usuario=usuario) --> dessa forma, os eventos serão filtrados por usuario
-    dados = {'eventos': evento}
+    evento = Evento.objects.filter(usuario=usuario) #filtragem por usuario
+    dados = {'eventos': evento} #dict
     return render(request, "agenda.html", dados)
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuario ou senha incorretos')
+    return redirect('/')
